5e085a8dba722e7bd109a68b816a3ca491549064
urmom790 <125672899+urmom790@users.noreply.github.com>
2023-02-17 08:26:58 -0600

    Revert "Bare server and Ultraviolet"

--- src/server.mjs
+++ src/server.mjs
@@ -1,5 +1,4 @@
-import { uvPath } from '@titaniumnetwork-dev/ultraviolet';
-import createBareServer from '@tomphttp/bare-server-node';
+import { Server } from '../bare/Server.mjs';
 import http from 'http';
 import path from 'path';
 import express from 'express';
@@ -7,7 +6,7 @@ import { readFile } from 'fs/promises';
 import pkg from './routes.mjs';
 import { paintSource, tryReadFile } from './randomization.mjs';
 
-const bare = createBareServer('/bare/');
+const bare = new Server('/bare/', '');
 const config = JSON.parse(await readFile(new URL('./config.json',
     import.meta.url)));
 const { pages, text404 } = pkg;
@@ -15,28 +14,14 @@ const __dirname = path.resolve();
 const port = process.env.PORT || config.port;
 const app = express();
 const router = express.Router();
-const server = http.createServer();
+const server = http.createServer(app);
 
-server.on('request', (req, res) => {
-    if (bare.shouldRoute(req)) {
-        bare.routeRequest(req, res);
-    } else {
-        app(req, res);
-    }
-});
-
-server.on('upgrade', (req, socket, head) => {
-    if (bare.shouldRoute(req)) {
-        bare.routeUpgrade(req, socket, head);
-    } else {
-        app(req, res);
-    }
-});
-
-router.get('/', async(req, res) => res.send(paintSource(tryReadFile(path.join(__dirname, 'views', ['/', '/?'].includes(req.url) ? pages.index : pages[Object.keys(req.query)[0]])))));
+router.get('/', async(req, res) => res.send(paintSource(tryReadFile(path.normalize(__dirname + '/views/' + (['/', '/?'].includes(req.url) ? pages.index : pages[Object.keys(req.query)[0]]))))));
 app.use(router);
-app.use(express.static(path.join(__dirname, 'views')));
-app.use("/uv/", express.static(uvPath));
+app.use('/', (req, res, next) => {
+    if (!bare.route_request(req, res)) return next()
+}); 
+app.use(express.static(path.normalize(__dirname + '/views')));
 app.disable('x-powered-by');
 app.use((req, res) => {
     res.status(404).send(paintSource(text404));
